31b1c4f2c08c404a6f68b6436212cc4e3c7d011b
Maksim Terentev <maxim.i.terentev@gmail.com>
2024-05-03 16:28:28 +0300

    created scripts/config.py

---
+++ scripts/config.py
@@ -0,0 +1,37 @@
+# Number of days
+N_DAYS = 7 #TODO: Make it a variable and be able to be changed in UI
+
+# Number of persons
+N_PERSONS = 4  # TODO: Make it a variable and be able to be changed in UI
+
+# Path to JSON file containing the recipe information
+JSON_MENU_PATH = './data/weekly_menu.json'
+
+# Path to the database
+DB_PATH = './data/recipes.db'
+
+meal_types = ['breakfast', 'lunch', 'dinner']
+
+"""
+Another option to share variables across multiple Python files within the same
+project is to use environment variables. Environment variables are system-wide
+variables that can be accessed by any process running on the system.
+
+Here's how you can use environment variables for your variables.
+    1. Set the environment variables in your shell environment (e.g., .bashrc,
+    .bash_profile, or .zshrc):
+
+    export N_PERSONS=4
+    export JSON_MENU_PATH='./data/weekly_menu.json'
+    export DB_PATH='./data/recipes.db'
+
+    2. In Python scripts, access the environment variables using the os.environ
+    dictionary:
+
+    import os
+
+    # Get the value of the environment variables
+    N_PERSONS = int(os.environ.get('N_PERSONS', 4))
+    JSON_MENU_PATH = os.environ.get('JSON_MENU_PATH', './data/weekly_menu.json')
+    DB_PATH = os.environ.get('DB_PATH', './data/recipes.db')
+"""
