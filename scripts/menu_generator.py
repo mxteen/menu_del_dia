fa97ebec4c6d70a916ef8ff83c6009e643c7511a
Maksim Terentev <maxim.i.terentev@gmail.com>
2024-05-03 14:28:08 +0300

    added messages generators: msg with list of dishes & msg with list of products

--- scripts/menu_generator.py
+++ scripts/menu_generator.py
@@ -1,21 +1,18 @@
 import sqlite3
 from datetime import datetime
 import json
-import os
 
+# Number of persons
+N_PERSONS = 4  #TODO: Make it a variable and be able to be changed in UI
+# Number of days
+N_DAYS = 7 #TODO: Make it a variable and be able to be changed in UI
 # Path to the database
-DB_PATH = '../data/recipes.db'
+DB_PATH = './data/recipes.db'
 # Path to json-file containing the recipe information
-JSON_MENU_PATH = '../data/weekly_menu.json'
+JSON_MENU_PATH = './data/weekly_menu.json'
 
-# Define SQLite query
-GET_RANDOM_RECIPES = """
-SELECT r.recipe_id
-FROM recipe r
-JOIN meal_type m ON r.meal_type_id = m.meal_type_id
-WHERE {}
-ORDER BY RANDOM() LIMIT ?
-"""
+meal_types = ['breakfast', 'lunch', 'dinner']
+meal_types_ru = ['Завтрак', 'Обед', 'Ужин']
 
 def lst_to_str(lst: list) -> str:
     """
@@ -25,8 +22,8 @@ def lst_to_str(lst: list) -> str:
         lst (list): The list of elements to be converted to a string.
 
     Returns:
-        str: A string representation of the input list with elements enclosed in parentheses
-             and separated by commas.
+        str: A string representation of the input list with elements enclosed 
+             in parentheses and separated by commas.
 
     Examples:
         >>> lst_to_str([40, 41, 31, 43, 20, 23, 38])
@@ -37,10 +34,21 @@ def lst_to_str(lst: list) -> str:
         '()'
 
     Note:
-        - If the input list is empty, an empty string with parentheses '()' will be returned.
-        - Elements in the list will be converted to their string representations using the str() function.
+        - If the input list is empty, an empty string with parentheses '()' 
+          will be returned.
+        - Elements in the list will be converted to their string 
+          representations using the str() function.
     """
     return '(' + ', '.join(map(str, lst)) + ')'
+
+# Define SQLite query
+GET_RANDOM_RECIPES = """
+SELECT r.recipe_id
+FROM recipe r
+JOIN meal_type m ON r.meal_type_id = m.meal_type_id
+WHERE {}
+ORDER BY RANDOM() LIMIT ?
+"""
 
 def generate_weekly_menu():
     """
@@ -68,18 +76,18 @@ def generate_weekly_menu():
     menu = dict()
     recipe_ids = list()
     # Meal types and corresponding conditions
-    meal_types = ['breakfast', 'lunch', 'dinner']
     where_conditions = [
         "m.meal_type_name = 'Завтрак'",
         "m.meal_type_name = 'Обед или ужин'",
-        f"m.meal_type_name = 'Обед или ужин' AND r.recipe_id NOT IN {lst_to_str(recipe_ids)}" 
+        "m.meal_type_name = 'Обед или ужин'" +\
+              f"AND r.recipe_id NOT IN {lst_to_str(recipe_ids)}"
     ]
 
     for meal_type, condition in zip(meal_types, where_conditions):
         # Execute SQL query to fetch random recipe IDs
         query = GET_RANDOM_RECIPES.format(condition)
         # Fetch recipe_ids and extract IDs from tuples
-        recipe_ids = [id[0] for id in cursor.execute(query, (7,)).fetchall()]
+        recipe_ids = [id[0] for id in cursor.execute(query, (N_DAYS,)).fetchall()]
         # Store the selected recipe IDs for the current meal type
         menu[meal_type] = recipe_ids
 
@@ -111,10 +119,110 @@ def write_menu_to_json(menu, path='weekly_menu.json'):
     with open(path, 'w') as file:
         json.dump(menu, file, indent=4)
 
+MAKE_LIST_OF_PRODUCTS = """
+SELECT 
+    i.ingredient_name, 
+    SUM(ri.ingredient_qty) * ? AS total_qty, 
+    m.measurement_name 
+FROM 
+    recipe_ingredients ri
+JOIN 
+    ingredients i ON ri.ingredient_id = i.ingredient_id
+JOIN 
+    measurements m ON ri.measurement_id = m.measurement_id
+WHERE {}
+GROUP BY 
+    ri.ingredient_id, m.measurement_name
+ORDER BY 
+    i.ingredient_name
+"""
+
+def make_list_of_products(recipe_ids: list) -> str:
+    """
+    Uses MAKE_LIST_OF_PRODUCTS query and returns multiple line string 
+    of ingredients with their quantities. Makes a list of products to buy.
+
+    Args:
+        recipe_ids (list): list of recipe IDs
+
+    Returns:
+        str: multiple line string with ingredients and their quantities and 
+             measurements
+    """
+    condition = "ri.recipe_id IN {}".format(lst_to_str(recipe_ids))
+    query = MAKE_LIST_OF_PRODUCTS.format(condition)
+
+    # Execute the SQL query to retrieve data
+    data = cursor.execute(query, (N_PERSONS,)).fetchall()
+
+    # Constructing the string representation of ingredients with their quantities and measurements
+    result_str = ""
+    for row in data:
+        ingredient_name, total_qty, measurement_name = row
+        result_str += f"{ingredient_name}: {total_qty} {measurement_name}\n"
+
+    return result_str
+
+MAKE_LIST_OF_DISHES = """
+SELECT recipe_name
+FROM recipe
+WHERE {}
+ORDER BY recipe_name
+"""
+
+def make_list_of_dishes(recipe_ids: list) -> str:
+    """
+     Uses MAKE_LIST_OF_DISHES query and returns multiple line string 
+    of ingredients with their quantities.  Makes a list of dishes for a week
+
+   Args:
+        recipe_ids (list): list of recipe IDs
+
+    Returns:
+        str: multiple line string with dishes
+    """
+    condition = "recipe_id IN {}".format(lst_to_str(recipe_ids))
+    query = MAKE_LIST_OF_DISHES.format(condition)
+
+    # Execute the SQL query to retrieve data
+    data = cursor.execute(query).fetchall()
+
+    # Constructing the string representation of ingredients with their quantities and measurements
+    result_str = ""
+    for row in data:
+        recipe_name = row[0]
+        result_str += f"{recipe_name}\n"
+
+    return result_str
+
 
 # Initialize SQLite database connection
+# TODO: развести процессы формирования menu и отправки сообщений в telegram
 with sqlite3.connect(DB_PATH) as connection:
     cursor = connection.cursor()
+    print('Generating menu...')  #TODO: писать в логи
     menu = generate_weekly_menu()
+    print('Menu generation complete') #TODO: писать в логи
 
+    print('Generating message with dishes list...') #TODO: писать в логи
+    message_with_recipe_names = ''
+    for meal_type, meal_type_ru in zip(meal_types, meal_types_ru):
+        message_with_recipe_names += meal_type_ru.upper() + '\n'
+        message_with_recipe_names += make_list_of_dishes(menu[meal_type]) + '\n'
+    print('Message generation complete') #TODO: писать в логи
+
+    print('Generating message with buying list...') #TODO: писать в логи
+    recipe_ids = []
+    for meal_type in meal_types:
+        recipe_ids += menu[meal_type]
+    message_with_buying_list = make_list_of_products(recipe_ids) + '\n'
+    print('Message generation complete') #TODO: писать в логи
+
+print('Sending messages...') #TODO: писать в логи
+print('\nСписок блюд')
+print(message_with_recipe_names) # TODO: send to telegram
+print('Список покупок')
+print(message_with_buying_list) # TODO: send to telegram
+print('Writing recipe IDs to temporary json-file')
 write_menu_to_json(menu, path=JSON_MENU_PATH)
+print()
